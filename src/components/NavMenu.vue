1960d49b93b67822ab6fb39401271f7135d54f4a
beautylotus <beautylotus718@gmail.com>
2023-09-08 17:11:14 -0700

    NavMenu commit

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,14 +1,8 @@
 <template>
    <nav style="width: 100%;">
-      <router-link style="font-size: 16px; padding: 10px; margin: 5px; background-color: #f0f0f0; text-decoration: none;" class="nav_link" to="/">Home</router-link>
-      <router-link style="font-size: 16px; padding: 10px; margin: 5px; background-color: #f0f0f0; text-decoration: none;" class="nav_link" to="/about">About</router-link>
+      <router-link style="font-size: 16px; padding: 10px; margin: 5px; background-color: #f0f0f0; text-decoration: none;"
+         class="nav_link" to="/">Home</router-link>
+      <router-link style="font-size: 16px; padding: 10px; margin: 5px; background-color: #f0f0f0; text-decoration: none;"
+         class="nav_link" to="/about">About</router-link>
    </nav>
 </template>
-
-<style>
-
-.nav_link {
-   width: 100px;
-}
-
-</style>
